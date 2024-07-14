bcc24e0c39792ce18ade00253543addd0c4828c1
centDiglesia <vincent.iglesia07@gmail.com>
2024-07-14 15:28:59 +0800

    fixed login

---
+++ productPage/product-page.js
@@ -0,0 +1,17 @@
+import "../styles/modern-noramalize.css";
+import "../styles/style.css";
+import "../styles/components/header.css";
+import "../styles/components/product.css"
+import "../styles/components/footer.css"
+
+import "../styles/utils.css";
+
+import displayRangeProducts from "../src/scripts/displayProductsRange";
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+    displayRangeProducts();
+   
+  
+  });  
