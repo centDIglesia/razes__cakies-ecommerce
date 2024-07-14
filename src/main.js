47efc6659696c0675b76f55466368912b99ad1ac
centDiglesia <vincent.iglesia07@gmail.com>
2024-07-14 13:42:39 +0800

    done feedback page

--- src/main.js
+++ src/main.js
@@ -5,15 +5,12 @@ import "../styles/components/hero.css";
 import "../styles/components/login.css";
 import "../styles/components/featured.css";
 import "../styles/components/product-range.css";
-import "../styles/components/popular.css"
+import "../styles/components/popular.css";
+import "../styles/components/chart.css";
+import "../styles/components/feedback.css"
 import "../styles/utils.css";
-import {
-  toggleForms,
-  togglePasswordVisibility,
-} from "/src/scripts/loginform-toggle";
-import { login } from "./scripts/loginAPI";
-import { signup } from "./scripts/signupAPI/";
-import { setupSlider } from './scripts/slider';
+
+import { setupSlider } from "./scripts/slider";
 import displayFeaturedProducts from "./scripts/displayFeatured";
 import displayRangeProducts from "./scripts/displayProductsRange";
 import displayPopularProducts from "./scripts/displayPopularProducts";
@@ -21,34 +18,23 @@ import displayPopularProducts from "./scripts/displayPopularProducts";
 setupSlider();
 
 document.addEventListener("DOMContentLoaded", () => {
-  
   displayFeaturedProducts();
   displayRangeProducts();
   displayPopularProducts();
-  toggleForms();
-  togglePasswordVisibility();
 
-  // Sign up button
-  const signupForm = document.getElementById("signup-form");
-  if (signupForm) {
-    signupForm.addEventListener("submit", async (event) => {
-      event.preventDefault();
-      await signup();
-    });
-  } else {
-    console.error("Signup form not found");
-  }
+//for viewing ful size chart
+  const viewFullChartbutton= document.querySelector(".view__sizechart");
+  const fullSizeChart = document.querySelector(".full__size-chart");
+  const closeButton = document.querySelector(".close");
 
-  // Login button
-  const loginForm = document.getElementById("login-form");
-  if (loginForm) {
-    loginForm.addEventListener("submit", async (event) => {
-      event.preventDefault();
-      await login();
-    });
-  } else {
-    console.error("Login form not found");
-  }
+  viewFullChartbutton.addEventListener("click", function(event) {
+    event.preventDefault();
+    fullSizeChart.classList.add("show-fullsizechart");
+  });
+
+  closeButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    fullSizeChart.classList.remove("show-fullsizechart");
+  });
+
 });
-
-
