6c54894b3630a248ef75c49e1da9a969b1c1dc9c
centDiglesia <vincent.iglesia07@gmail.com>
2024-07-20 23:33:42 +0800

    added cart info

--- productPage/product-page.js
+++ productPage/product-page.js
@@ -7,43 +7,13 @@ import "../styles/utils.css";
 
 import {
   displayAllProducts,
-  showaddtoCartForm,
-  updatePrice
+  showCustomizeForm,
+  showDefaultForm
 } from "../src/scripts/displayProducts";
 
 document.addEventListener("DOMContentLoaded", () => {
   displayAllProducts();
-  showaddtoCartForm();
-
-  setTimeout(setupImageUpload, 1);
-});
-
-function setupImageUpload() {
-  let refImage = document.getElementById("ref-image");
-  let refImageInput = document.getElementById("input__reference");
-
-  if (refImage && refImageInput) {
-    refImageInput.addEventListener("change", function () {
-      if (this.files && this.files[0]) {
-        let imageUrl = URL.createObjectURL(this.files[0]);
-        refImage.src = imageUrl;
-        refImage.onload = function () {
-          URL.revokeObjectURL(imageUrl);
-        };
-      }
-    });
-  } else {
-    console.error("Reference image or input is null");
-  }
-}
-document.addEventListener("change", function (event) {
-  if (event.target && event.target.id === "setting-layer") {
-    updatePrice('setting-layer', '.setting-layer-price');
-  }
-  if (event.target && event.target.id === "setting-top") {
-    updatePrice('setting-top', '.setting-top-price');
-  }
-  if (event.target && event.target.id === "setting-side") {
-    updatePrice('setting-side', '.setting-side-price');
-  }
+  showDefaultForm();
+  showCustomizeForm();
+ 
 });
