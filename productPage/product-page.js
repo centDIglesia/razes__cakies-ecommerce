e83c4840b5af515b55171c0b996f109ff22d345c
centDiglesia <vincent.iglesia07@gmail.com>
2024-07-22 01:54:06 +0800

    added filter products

--- productPage/product-page.js
+++ productPage/product-page.js
@@ -19,3 +19,28 @@ document.addEventListener("DOMContentLoaded", () => {
   showCustomizeForm();
   showMobileNav();
 });
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const categoryButtons = document.querySelectorAll('.category__btn');
+  const productsButtons = document.querySelectorAll('.product-range__card-cake');
+
+  function addActiveClass(buttons, activeClass) {
+    buttons.forEach(button => {
+      button.addEventListener('click', function(event) {
+        event.preventDefault(); 
+        buttons.forEach(btn => btn.classList.remove(activeClass));
+
+     
+        this.classList.add(activeClass);
+
+        // Call displayAllProducts to update the product list
+        displayAllProducts();
+      });
+    });
+  }
+
+  addActiveClass(categoryButtons, 'category__btn-active');
+  addActiveClass(productsButtons, 'product-range__card-cake-active');
+});
