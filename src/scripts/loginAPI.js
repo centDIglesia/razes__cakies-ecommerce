c1dcbbb4edc783b437dff0da446405adc3ddd846
krl1403 <kennethlavilla1403@gmail.com>
2024-07-21 16:31:01 +0800

    modified get user

--- src/scripts/loginAPI.js
+++ src/scripts/loginAPI.js
@@ -7,7 +7,7 @@ export async function login() {
     try {
         const user = {
             // Add placeholder here coz email at pass lang need
-            firstName: "string", 
+            firstName: "string",
             lastName: "string",
             email: email,
             password: password
@@ -27,14 +27,40 @@ export async function login() {
         }
 
         const data = await response.json();
-        localStorage.setItem('token', data.token);
+        const decodedToken = parseJwt(data.token);
+        // console.log('Decoded Token:', decodedToken); 
+
+        const emailClaim = 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress';
+        const emailFromApi = decodedToken[emailClaim];
+        const firstName = decodedToken.firstName || 'Unknown';
+        const lastName = decodedToken.lastName || 'Unknown';
+
+        const currentUser = {
+            userEmail: emailFromApi,
+            userFN: firstName,
+            userLN: lastName
+        };
+        // Save the user to the local storage
+        localStorage.setItem("currentUser", JSON.stringify(currentUser));
+
+        console.log(localStorage.getItem("currentUser"));
         alert("Logged in successful");
     } catch (error) {
         console.error('Error:', error);
         alert("Invalid Credentials");
-    }
-    finally {
+    } finally {
         document.getElementById('loginEmail').value = '';
         document.getElementById('loginPassword').value = '';
     }
+}
+
+// function to parse the jwt token sent from the api
+function parseJwt(token) {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
 }
