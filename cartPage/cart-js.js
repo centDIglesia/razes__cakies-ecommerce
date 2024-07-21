78bf3222836eb418ae5dc930f36744edfa14949e
centDiglesia <vincent.iglesia07@gmail.com>
2024-07-22 00:23:38 +0800

    csacs

--- cartPage/cart-js.js
+++ cartPage/cart-js.js
@@ -3,6 +3,7 @@ import "../styles/style.css";
 import "../styles/components/header.css";
 import "../styles/components/footer.css";
 import "../styles/components/cart.css";
+import "../styles/components/mobile-nav.css";
 import "../styles/utils.css";
 
 import { cart } from "../src/Data/cart";
@@ -11,10 +12,12 @@ import {
   loadOrderSummary,
 } from "../src/scripts/displayCartItems";
 
+import showMobileNav from "../src/scripts/displayNavBar";
+
 // Retrieve the customized form data from localStorage
 document.addEventListener("DOMContentLoaded", () => {
   loadOrderSummary();
-
+  showMobileNav();
   const storedCart = localStorage.getItem("cart");
 
   if (storedCart) {
--- trackOrderPage/track-order-js.js
+++ trackOrderPage/track-order-js.js
@@ -3,7 +3,11 @@ import "../styles/components/footer.css";
 import "../styles/components/order.css";
 import "../styles/components/track-order.css";
 import "../styles/utils.css";
+import "../styles/components/mobile-nav.css"
 import "../styles/modern-noramalize.css";
 import "../styles/style.css";
 
+import showMobileNav from "../src/scripts/displayNavBar";
 
+
+showMobileNav();
